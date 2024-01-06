20e1716101f1c3114fe564f7483d7915dac76004
Thalisson Castro <adaothalisson@hotmail.com>
2024-01-06 18:02:36 -0300

    insert a logo image to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import logo from '../assets/ignite-logo.svg'
 
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>Feed Thalisson</strong>
+            <img src={logo} />
         </header>
     )
 }
