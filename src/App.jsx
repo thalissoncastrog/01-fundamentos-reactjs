e852448cf9133bc5a03d9ecafddea45ddfe73696
Thalisson Castro <adaothalisson@hotmail.com>
2024-01-06 17:15:23 -0300

    Applied css to a single component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
 import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post 
         author="Thalisson" 
         content="This is my middle name."
